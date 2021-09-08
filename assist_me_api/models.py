12c5c11273c19201479b2723294ca9a94b2ed226
Tommy Chung <tommychung@TC-MacBook-Pro.local>
2021-09-08 06:37:05 -0400

    Update

--- assist_me_api/models.py
+++ assist_me_api/models.py
@@ -2,27 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Budget(models.Model):
-    income = models.IntegerField(null=True, blank=True)
     name = models.CharField(max_length=32)
-    cost = models.IntegerField(null=True, blank=True)
+    cost = models.IntegerField(null=True)
     note = models.CharField(max_length=400, blank=True)
 
 class Expense(models.Model):
-    income = models.IntegerField(null=True, blank=True)
     name = models.CharField(max_length=32)
-    cost = models.IntegerField(null=True, blank=True)
+    cost = models.IntegerField(null=True)
     note = models.CharField(max_length=400, blank=True)
 
 class Task(models.Model):
     name = models.CharField(max_length=32)
-    time = models.IntegerField(null=True, blank=True)
+    time = models.IntegerField(null=True)
     note = models.CharField(max_length=400, blank=True)
 
 class Daily(models.Model):
     name = models.CharField(max_length=32)
-    time = models.IntegerField(null=True, blank=True)
+    time = models.IntegerField(null=True)
     note = models.CharField(max_length=400, blank=True)
-    due = models.DateField(null=True, blank=True)
+    due = models.DateField(null=True)
 
 class User(models.Model):
     username = models.CharField(max_length=75, unique=True)
